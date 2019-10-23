5d63c361750ef2cc0b94e0429df02385288d6e2a
Trueblood <mattjtrueblood@gmail.com>
2019-10-23 15:52:38 -0400

    Added shrubs and grass

--- src/main/java/huntingrl/ecs/components/PositionComponent.java
+++ src/main/java/huntingrl/ecs/components/PositionComponent.java
@@ -1,15 +1,13 @@
 package huntingrl.ecs.components;
 
 import com.badlogic.ashley.core.Component;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @AllArgsConstructor
 @Builder
 @Getter
 @Setter
+@EqualsAndHashCode
 public class PositionComponent implements Component {
     //x and y are in world coords
     private long x;
--- src/main/java/huntingrl/world/World.java
+++ src/main/java/huntingrl/world/World.java
@@ -3,13 +3,17 @@ package huntingrl.world;
 import com.badlogic.ashley.core.Entity;
 import com.flowpowered.noise.Noise;
 import com.flowpowered.noise.NoiseQuality;
+import huntingrl.ecs.ComponentMappers;
 import huntingrl.ecs.components.*;
 import huntingrl.util.Math.Point;
 import lombok.Getter;
 
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Getter
 public class World {
@@ -17,6 +21,8 @@ public class World {
     private final int elevationSeed;
     private final int treeSeed;
     private final int humiditySeed;
+    private final int shrubSeed;
+    private final int grassSeed;
 
     public static final short WATER_ELEVATION = 50;
 
@@ -30,10 +36,19 @@ public class World {
     private static final double MIN_HUMIDITY_FOR_TREES_TO_APPEAR = 0.15;
     private static final double TREE_NOISE_FREQUENCY = 0.5;
 
+    private static final long SHRUB_MIN_DISTANCE = 3;
+    private static final double SHRUB_NOISE_FREQUENCY = 0.5;
+
+    private static final long GRASS_MIN_DISTANCE = 1;
+    private static final double GRASS_NOISE_FREQUENCY = 0.5;
+
+
     public World(int seed) {
         this.elevationSeed = seed;
         treeSeed = elevationSeed + 100;
         humiditySeed = treeSeed + 100;
+        shrubSeed = humiditySeed + 100;
+        grassSeed = shrubSeed + 100;
     }
 
     public WorldPoint pointAt(long x, long y, short scale) {
@@ -66,7 +81,20 @@ public class World {
     }
 
     public List<Entity> entitiesInBounds(long minX, long minY, long maxX, long maxY) {
-        return treesInBounds(minX, minY, maxX, maxY);
+        List<Entity> entities = new ArrayList<Entity>();
+        List<Entity> trees = treesInBounds(minX, minY, maxX, maxY);
+        List<Entity> shrubs = shrubsInBounds(minX, minY, maxX, maxY);
+        List<Entity> grass = grassInBounds(minX, minY, maxX, maxY);
+
+        //prioritize trees > shrubs > grass.  Delete shrubs or grass that are on tree tiles, and grass that is on shrub tiles
+        removeDuplicatePositionEntities(trees, shrubs);
+        removeDuplicatePositionEntities(trees, grass);
+        removeDuplicatePositionEntities(shrubs, grass);
+
+        entities.addAll(trees);
+        entities.addAll(shrubs);
+        entities.addAll(grass);
+        return entities;
     }
 
     private List<Entity> treesInBounds(long minX, long minY, long maxX, long maxY) {
@@ -77,16 +105,15 @@ public class World {
                 if (elevationAtCoords(xc, yc) > WATER_ELEVATION && humidityAtThisTile > MIN_HUMIDITY_FOR_TREES_TO_APPEAR) {
                     double bestVal = 0;
                     long treeDistance = TREE_DISTANCE_AT_MIN_HUMIDITY  - Math.round(humidityAtThisTile * Math.abs(TREE_DISTANCE_AT_MAX_HUMIDITY - TREE_DISTANCE_AT_MIN_HUMIDITY));
-                    System.out.println(treeDistance);
                     for (long xn = xc - treeDistance; xn < xc + treeDistance; xn++) {
                         for (long yn = yc - treeDistance; yn < yc + treeDistance; yn++) {
-                            double e = treeNoiseValueAt(xn, yn);
+                            double e = generateValueFromSingleFrequencyNoise(xn, yn, treeSeed, TREE_NOISE_FREQUENCY);
                             if (e > bestVal) {
                                 bestVal = e;
                             }
                         }
                     }
-                    if(treeNoiseValueAt(xc, yc) == bestVal) {
+                    if(generateValueFromSingleFrequencyNoise(xc, yc, treeSeed, TREE_NOISE_FREQUENCY) == bestVal) {
                         trees.add(generateTree(new Point(xc, yc)));
                     }
                 }
@@ -95,10 +122,56 @@ public class World {
         return trees;
     }
 
-    private double treeNoiseValueAt(long x, long y) {
+    private List<Entity> shrubsInBounds(long minX, long minY, long maxX, long maxY) {
+        List<Entity> shrubs = new ArrayList<>();
+        for(long xc = minX; xc < maxX; xc++) {
+            for(long yc = minY; yc < maxY; yc++) {
+                if (elevationAtCoords(xc, yc) > WATER_ELEVATION) {
+                    double bestVal = 0;
+                    for (long xn = xc - SHRUB_MIN_DISTANCE; xn < xc + SHRUB_MIN_DISTANCE; xn++) {
+                        for (long yn = yc - SHRUB_MIN_DISTANCE; yn < yc + SHRUB_MIN_DISTANCE; yn++) {
+                            double e = generateValueFromSingleFrequencyNoise(xn, yn, shrubSeed, SHRUB_NOISE_FREQUENCY);
+                            if (e > bestVal) {
+                                bestVal = e;
+                            }
+                        }
+                    }
+                    if(generateValueFromSingleFrequencyNoise(xc, yc, shrubSeed, SHRUB_NOISE_FREQUENCY) == bestVal) {
+                        shrubs.add(generateShrub(new Point(xc, yc)));
+                    }
+                }
+            }
+        }
+        return shrubs;
+    }
+
+    private List<Entity> grassInBounds(long minX, long minY, long maxX, long maxY) {
+        List<Entity> grass = new ArrayList<>();
+        for(long xc = minX; xc < maxX; xc++) {
+            for(long yc = minY; yc < maxY; yc++) {
+                if (elevationAtCoords(xc, yc) > WATER_ELEVATION) {
+                    double bestVal = 0;
+                    for (long xn = xc - GRASS_MIN_DISTANCE; xn < xc + GRASS_MIN_DISTANCE; xn++) {
+                        for (long yn = yc - GRASS_MIN_DISTANCE; yn < yc + GRASS_MIN_DISTANCE; yn++) {
+                            double e = generateValueFromSingleFrequencyNoise(xn, yn, grassSeed, GRASS_NOISE_FREQUENCY);
+                            if (e > bestVal) {
+                                bestVal = e;
+                            }
+                        }
+                    }
+                    if(generateValueFromSingleFrequencyNoise(xc, yc, grassSeed, GRASS_NOISE_FREQUENCY) == bestVal) {
+                        grass.add(generateGrass(new Point(xc, yc)));
+                    }
+                }
+            }
+        }
+        return grass;
+    }
+
+    private double generateValueFromSingleFrequencyNoise(long x, long y, int seed, double frequency) {
         return Noise.gradientCoherentNoise3D(
-                x * TREE_NOISE_FREQUENCY, y * TREE_NOISE_FREQUENCY,
-                0, treeSeed, NoiseQuality.STANDARD);
+                x * frequency, y * frequency,
+                0, seed, NoiseQuality.STANDARD);
     }
 
     private Entity generateTree(Point point) {
@@ -109,6 +182,40 @@ public class World {
         entity.add(new BlocksMovementComponent());
         entity.add(new CastsShadowComponent());
         return entity;
+    }
+
+    private Entity generateShrub(Point point) {
+        Entity entity = new Entity();
+        entity.add(new PositionComponent(point.getX(), point.getY()));
+        entity.add(new GraphicsComponent((char) 5,
+                new Color(0, 60, 0), null));
+        entity.add(new LocalOnlyComponent());
+        return entity;
+    }
+
+    private Entity generateGrass(Point point) {
+        Entity entity = new Entity();
+        entity.add(new PositionComponent(point.getX(), point.getY()));
+        entity.add(new GraphicsComponent((char) 34,
+                new Color(0,
+                        200,
+                        0), null));
+        entity.add(new LocalOnlyComponent());
+        return entity;
+    }
+
+    private void removeDuplicatePositionEntities(List<Entity> primaryList, List<Entity> secondaryList) {
+        List<Entity> entitiesToDeleteFromSecondaryList = new ArrayList<>();
+        for(Entity secondaryEntity: secondaryList) {
+            for(Entity primaryEntity: primaryList) {
+                PositionComponent primaryPosition = ComponentMappers.positionMapper.get(primaryEntity);
+                PositionComponent secondaryPosition = ComponentMappers.positionMapper.get(secondaryEntity);
+                if(primaryPosition.equals(secondaryPosition)) {
+                    entitiesToDeleteFromSecondaryList.add(secondaryEntity);
+                }
+            }
+        }
+        secondaryList.removeAll(entitiesToDeleteFromSecondaryList);
     }
 
 }
